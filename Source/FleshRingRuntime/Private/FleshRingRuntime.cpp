38ac9b8c2fc7ab0c2213df84044cd71d6bc21ebd
Teterschaft <gofla2242@tukorea.ac.kr>
2025-12-22 21:57:39 +0900

    [Teterschaft][Featrue] 플러그인에 Shaders 폴더 및 셰이더 가상 경로 설정
    [git-p4: depot-paths = "//depot/FleshRing/Plugins/FleshRingPlugin/": change = 25]

--- Source/FleshRingRuntime/Private/FleshRingRuntime.cpp
+++ Source/FleshRingRuntime/Private/FleshRingRuntime.cpp
@@ -1,18 +1,31 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "FleshRingRuntime.h"
+#include "Interfaces/IPluginManager.h"
+#include "Misc/Paths.h"
+#include "ShaderCore.h"
 
 #define LOCTEXT_NAMESPACE "FFleshRingRuntimeModule"
 
 void FFleshRingRuntimeModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+    // 셰이더 가상 경로 등록
+    FString PluginShaderDir = FPaths::Combine(
+        IPluginManager::Get().FindPlugin(TEXT("FleshRingPlugin"))->GetBaseDir(),
+        TEXT("Shaders")
+    );
+    AddShaderSourceDirectoryMapping(TEXT("/FleshRingPlugin"), PluginShaderDir);
 }
 
 void FFleshRingRuntimeModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+
+    // 셰이더 경로 해제
+    ResetAllShaderSourceDirectoryMappings();
 }
 
 #undef LOCTEXT_NAMESPACE
